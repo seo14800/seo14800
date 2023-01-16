c85bbdec77a3a35cf22374afc3b48dca977f4200
seo14800 <seo14800@gmail.com>
2023-01-16 17:44:13 +0900

    add code

---
+++ programmers/choice_mandarin/mandarin.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int k, vector<int> tangerine) {
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    return 0;
+}
